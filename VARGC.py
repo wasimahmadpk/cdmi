743feffee0e3ffc7264eabbc1b9d518ffce5bc9b
Wasim <wasimahmad.qc@gmail.com>
2022-06-21 13:27:31 +0200

    Updated hyperparams

--- VARGC.py
+++ VARGC.py
@@ -68,21 +68,21 @@ win_size = 1
 # vpd = normalize(down_sample(ovpd, win_size))
 # print("Length:", len(rg))
 
-# "Load synthetic data"
-syndata = pd.read_csv("/home/ahmad/PycharmProjects/deepCausality/datasets/ncdata/synthetic_data.csv", sep=',')
+# # "Load synthetic data"
+# syndata = pd.read_csv("/home/ahmad/PycharmProjects/deepCausality/datasets/ncdata/synthetic_data.csv", sep=',')
+#
+# x1 = normalize(down_sample(np.array(syndata['Z1']), win_size))
+# x2 = normalize(down_sample(np.array(syndata['Z2']), win_size))
+# x3 = normalize(down_sample(np.array(syndata['Z3']), win_size))
+# x4 = normalize(down_sample(np.array(syndata['Z4']), win_size))
+# x5 = normalize(down_sample(np.array(syndata['Z5']), win_size))
 
-x1 = normalize(down_sample(np.array(syndata['Z1']), win_size))
-x2 = normalize(down_sample(np.array(syndata['Z2']), win_size))
-x3 = normalize(down_sample(np.array(syndata['Z3']), win_size))
-x4 = normalize(down_sample(np.array(syndata['Z4']), win_size))
-x5 = normalize(down_sample(np.array(syndata['Z5']), win_size))
-
-
-# dataobj = RiverData()
-# data = dataobj.get_data()
-# kts = data['Kempten']
-# dts = data['Dillingen']
-# lts = data['Lenggries']
+dataobj = RiverData()
+data = dataobj.get_data()
+print(data.head(5))
+dts = data.iloc[:, 1].tolist()
+kts = data.iloc[:, 2].tolist()
+lts = data.iloc[:, 3].tolist()
 
 # # "Load semi-synthetic data"
 # syndata = pd.read_csv("/home/ahmad/PycharmProjects/deepCause/datasets/ncdata/artificial_data.csv", sep=',')
@@ -92,14 +92,13 @@ x5 = normalize(down_sample(np.array(syndata['Z5']), win_size))
 # reco = normalize(down_sample(np.array(syndata['Reco']), win_size))
 #
 #
-col_list = ['Z1', 'Z2', 'Z3', 'Z4', 'Z5']
-
-# # col_list = ['Kts', 'Dts', 'Lts']
+# col_list = ['Z1', 'Z2', 'Z3', 'Z4', 'Z5']
+col_list = ['Kts', 'Dts', 'Lts']
 # col_list = ['Rg', 'T', 'GPP', 'Reco']
-dt = {'Z1': x1, 'Z2': x2, 'Z3': x3, 'Z4': x4, 'Z5': x5}
+# dt = {'Z1': x1, 'Z2': x2, 'Z3': x3, 'Z4': x4, 'Z5': x5}
 
-data = pd.DataFrame(dt, columns=col_list)
-# # data = pd.DataFrame({'Kts': kts, 'Dts': dts, 'Lts': lts}, columns=col_list)
+# data = pd.DataFrame(dt, columns=col_list)
+data = pd.DataFrame({'Kts': kts, 'Dts': dts, 'Lts': lts}, columns=col_list)
 # data = pd.DataFrame({'Rg': rg[0:1000], 'T': temp[0:1000], 'GPP': gpp[0: 1000], 'Reco': reco[0: 1000]}, columns=col_list)
 # print(data.head(100))
 
