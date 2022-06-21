155f517c21b3c8baef5e7322079db051687b5fa1
Wasim <wasimahmad.qc@gmail.com>
2022-06-21 13:26:17 +0200

    New experiments with LINGAM

--- LINGAM.py
+++ LINGAM.py
@@ -64,15 +64,22 @@ win_size = 1
 # vpd = normalize(down_sample(ovpd, win_size))
 # print("Length:", len(rg))
 
-# "Load synthetic data"
-syndata = pd.read_csv("/home/ahmad/PycharmProjects/deepCausality/datasets/ncdata/synthetic_data.csv", sep=',')
+dataobj = RiverData()
+data = dataobj.get_data()
 
-x1 = normalize(down_sample(np.array(syndata['Z1']), win_size))
-x2 = normalize(down_sample(np.array(syndata['Z2']), win_size))
-x3 = normalize(down_sample(np.array(syndata['Z3']), win_size))
-x4 = normalize(down_sample(np.array(syndata['Z4']), win_size))
-x5 = normalize(down_sample(np.array(syndata['Z5']), win_size))
+dts = data.iloc[:, 1].tolist()
+kts = data.iloc[:, 2].tolist()
+lts = data.iloc[:, 3].tolist()
 
+
+# # "Load synthetic data"
+# syndata = pd.read_csv("/home/ahmad/PycharmProjects/deepCausality/datasets/ncdata/synthetic_data.csv", sep=',')
+#
+# x1 = normalize(down_sample(np.array(syndata['Z1']), win_size))
+# x2 = normalize(down_sample(np.array(syndata['Z2']), win_size))
+# x3 = normalize(down_sample(np.array(syndata['Z3']), win_size))
+# x4 = normalize(down_sample(np.array(syndata['Z4']), win_size))
+# x5 = normalize(down_sample(np.array(syndata['Z5']), win_size))
 
 # # "Load semi-synthetic data"
 # syndata = pd.read_csv("/home/ahmad/PycharmProjects/deepCause/datasets/ncdata/artificial_data.csv", sep=',')
@@ -82,14 +89,14 @@ x5 = normalize(down_sample(np.array(syndata['Z5']), win_size))
 # reco = normalize(down_sample(np.array(syndata['Reco']), win_size))
 #
 #
-col_list = ['Z1', 'Z2', 'Z3', 'Z4', 'Z5']
+# col_list = ['Z1', 'Z2', 'Z3', 'Z4', 'Z5']
 
-# # col_list = ['Kts', 'Dts', 'Lts']
+col_list = ['Kts', 'Dts', 'Lts']
 # col_list = ['Rg', 'T', 'GPP', 'Reco']
-dt = {'Z1': x1, 'Z2': x2, 'Z3': x3, 'Z4': x4, 'Z5': x5}
+# dt = {'Z1': x1, 'Z2': x2, 'Z3': x3, 'Z4': x4, 'Z5': x5}
 
-data = pd.DataFrame(dt, columns=col_list)
-# # data = pd.DataFrame({'Kts': kts, 'Dts': dts, 'Lts': lts}, columns=col_list)
+# data = pd.DataFrame(dt, columns=col_list)
+data = pd.DataFrame({'Kts': normalize(kts[:700]), 'Dts': normalize(dts[:700]), 'Lts': normalize(lts[:700])}, columns=col_list)
 # data = pd.DataFrame({'Rg': rg[0:1000], 'T': temp[0:1000], 'GPP': gpp[0: 1000], 'Reco': reco[0: 1000]}, columns=col_list)
 # print(data.head(100))
 
@@ -127,7 +134,7 @@ data = pd.DataFrame(dt, columns=col_list)
 model = lingam.DirectLiNGAM()
 # model.fit(data)
 result = model.bootstrap(data, n_sampling=5)
-cdc = result.get_causal_direction_counts(n_directions=10, min_causal_effect=0.01, split_by_causal_effect_sign=True)
+cdc = result.get_causal_direction_counts(n_directions=8, min_causal_effect=0.05, split_by_causal_effect_sign=True)
 print_causal_directions(cdc, 5)
 
 # # Using the causal_order_ properties,
