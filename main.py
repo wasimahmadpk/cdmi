b546bba845a03597cde852d40d7d952787dfd0b4
Wasim <wasimahmad.qc@gmail.com>
2022-07-01 20:31:12 +0200

    methods moved to preprocessing

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import math
-# import netCDF
 import pickle
 import random
 import pathlib
+import parameters
 import numpy as np
 import mxnet as mx
 import pandas as pd
 from os import path
+import preprocessing
 from math import sqrt
-from netCDF4 import Dataset
 from itertools import islice
 from datetime import datetime
 from riverdata import RiverData
-from deepcauses import deepCause
+from deepcause import deepCause
 import matplotlib.pyplot as plt
 from knockoffs import Knockoffs
 from scipy.special import stdtr
-from model_test import modelTest
+from forecast import modelTest
 from gluonts.trainer import Trainer
 from gluonts.evaluation import Evaluator
 from sklearn.metrics import mean_squared_error
@@ -31,62 +31,10 @@ np.random.seed(1)
 mx.random.seed(2)
 
 
-def deseasonalize(var, interval):
-
-    deseasonalize_data = []
-    for i in range(interval, len(var)):
-        value = var[i] - var[i - interval]
-        deseasonalize_data.append(value)
-    return deseasonalize_data
-
-
-def normalize(var):
-    nvar = (np.array(var) - np.mean(var)) / np.std(var)
-    return nvar
-
-
-def down_sample(data, win_size, partition=None):
-    agg_data = []
-    daily_data = []
-    for i in range(len(data)):
-        daily_data.append(data[i])
-
-        if (i % win_size) == 0:
-
-            if partition == None:
-                agg_data.append(sum(daily_data) / win_size)
-                daily_data = []
-            elif partition == 'gpp':
-                agg_data.append(sum(daily_data[24: 30]) / 6)
-                daily_data = []
-            elif partition == 'reco':
-                agg_data.append(sum(daily_data[40: 48]) / 8)
-                daily_data = []
-    return agg_data
-
-
-def SNR(s, n):
-    Ps = np.sqrt(np.mean(np.array(s) ** 2))
-    Pn = np.sqrt(np.mean(np.array(n) ** 2))
-    SNR = Ps / Pn
-    return 10 * math.log(SNR, 10)
-
-
-def mean_absolute_percentage_error(y_true, y_pred):
-
-    return np.mean(np.abs((y_true - y_pred) / y_true))
-
-
-def running_avg_effect(y, yint):
-
-    rae = 0
-    for i in range(len(y)):
-        ace = 1/((params.get("train_len") + 1 + i) - params.get("train_len")) * (rae + (y[i] - yint[i]))
-    return rae
-
-
+prep = preprocessing()
 # Parameters for River discharge data
-
+real_pars = parameters.get_real_params()
+syn_pars = parameters.get_syn_params()
 freq = 'D'
 dim = 3
 epochs = 150
@@ -151,7 +99,7 @@ lenggries = data.iloc[:, 3].tolist()
 # data = {'Rg': rg[8000:12000], 'T': temp[8000:12000], 'GPP': gpp[8000:12000], 'Reco': reco[8000:12000]}
 # df = pd.DataFrame(data, columns=['Rg', 'T', 'GPP', 'Reco'])
 
-data = {'Kt': normalize(kempton), 'Dt': normalize(dillingen), 'Lt': normalize(lenggries)}
+data = {'Kt': prep.normalize(kempton), 'Dt': prep.normalize(dillingen), 'Lt': prep.normalize(lenggries)}
 df = pd.DataFrame(data, columns=['Kt', 'Dt', 'Lt'])
 # /////////////////////////////////////////////////////////////
 original_data = []
