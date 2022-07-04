dabf07b163197ac58548f17df6750df00aeac0b5
Wasim <wasimahmad.qc@gmail.com>
2022-07-04 14:24:08 +0200

    code shifted to preprocessing and params

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import numpy as np
 import mxnet as mx
 import pandas as pd
 from os import path
-import preprocessing
 from math import sqrt
 from itertools import islice
+import preprocessing as prep
 from datetime import datetime
 from riverdata import RiverData
 from deepcause import deepCause
@@ -30,86 +30,36 @@ from gluonts.distribution.multivariate_gaussian import MultivariateGaussianOutpu
 np.random.seed(1)
 mx.random.seed(2)
 
-
-prep = preprocessing()
 # Parameters for River discharge data
-real_pars = parameters.get_real_params()
-syn_pars = parameters.get_syn_params()
-freq = 'D'
-dim = 3
-epochs = 150
-win_size = 1
+pars = parameters.get_real_params()
+freq = pars.get("freq")
+epochs = pars.get("epochs")
+win_size = pars.get("win_size")
+training_length = pars.get("train_len")
+prediction_length = pars.get("pred_len")
+num_samples = pars.get("num_samples")
+num_layers = pars.get("num_layers")
+num_cells = pars.get("num_cells")
+dropout_rate = pars.get("dropout_rate")
+batch_size = pars.get("batch_size")
 
-# # Parameters for synthetic data
-# freq = '30min'
-# epochs = 150
-# win_size = 1
-
-training_length = 555    # 555
-prediction_length = 28   # 28
-num_samples = 10
-
-# # Parameters for ecosystem data
-# freq = '30min'
-# dim = 4
-# epochs = 150
-# win_size = 1
-# #
-# training_length = 666
-# prediction_length = 48
-# num_samples = 10
-
+# data preprocessing
+prep = preprocessing()
 
 # Load river discharges data
-dataobj = RiverData()
-data = dataobj.get_data()
+df = load_river_data()
 
-dillingen = data.iloc[:, 1].tolist()
-kempton = data.iloc[:, 2].tolist()
-# kempton = [0, 0, 0, 0, 0] + kempton
-lenggries = data.iloc[:, 3].tolist()
-
-# Load synthetic data *************************
-# df = pd.read_csv("/home/ahmad/PycharmProjects/deepCausality/datasets/ncdata/synthetic_data.csv")
-
-
-# # "Load fluxnet 2015 data for grassland IT-Mbo site"
-# fluxnet = pd.read_csv("/home/ahmad/PycharmProjects/deepCausality/datasets/fluxnet2015/FLX_IT-MBo_FLUXNET2015_SUBSET_2003-2013_1-4/FLX_IT-MBo_FLUXNET2015_SUBSET_HH_2003-2013_1-4.csv")
-# org = fluxnet['SW_IN_F']
-# otemp = fluxnet['TA_F']
-# ovpd = fluxnet['VPD_F']
-# # oppt = fluxnet['P_F']
-# # nee = fluxnet['NEE_VUT_50']
-# ogpp = fluxnet['GPP_NT_VUT_50']
-# oreco = fluxnet['RECO_NT_VUT_50']
-# #
-# # ************* LOad FLUXNET2015 data ************************
-
-# rg = normalize(down_sample(org, win_size))
-# temp = normalize(down_sample(otemp, win_size))
-# # gpp = normalize(down_sample(nee, win_size, partition='gpp'))
-# # reco = normalize(down_sample(nee, win_size, partition='reco'))
-# gpp = normalize(down_sample(ogpp, win_size))
-# reco = normalize(down_sample(oreco, win_size))
-# # ppt = normalize(down_sample(oppt, win_size))
-# vpd = normalize(down_sample(ovpd, win_size))
-# # swc = normalize(down_sample(oswc, win_size))
-# # heat = normalize(down_sample(oheat, win_size))
-#
-# data = {'Rg': rg[8000:12000], 'T': temp[8000:12000], 'GPP': gpp[8000:12000], 'Reco': reco[8000:12000]}
-# df = pd.DataFrame(data, columns=['Rg', 'T', 'GPP', 'Reco'])
-
-data = {'Kt': prep.normalize(kempton), 'Dt': prep.normalize(dillingen), 'Lt': prep.normalize(lenggries)}
-df = pd.DataFrame(data, columns=['Kt', 'Dt', 'Lt'])
-# /////////////////////////////////////////////////////////////
 original_data = []
 dim = len(df.columns)
+columns = df.columns
 print(f"Dimension {dim} and Columns: {df.columns}")
 
 for col in df:
     original_data.append(df[col])
 
 original_data = np.array(original_data)
+
+# training set
 train_ds = ListDataset(
     [
         {'start': "01/01/1961 00:00:00",
@@ -125,18 +75,19 @@ estimator = DeepAREstimator(
     prediction_length=prediction_length,
     context_length=prediction_length,
     freq=freq,
-    num_layers=3,  # 3
-    num_cells=33,  # 33
-    dropout_rate=0.1,
+    num_layers=num_layers,  # 3
+    num_cells=num_cells,  # 33
+    dropout_rate=dropout_rate,
     trainer=Trainer(
         ctx="cpu",
         epochs=epochs,
         hybridize=False,
-        batch_size=32
+        batch_size=batch_size
     ),
     distr_output=MultivariateGaussianOutput(dim=dim)
 )
 
+# load model if not already trained
 # model_path = "models/trained_model_syn08Jun.sav"
 model_path = "models/trained_model_river16Jun.sav"
 filename = pathlib.Path(model_path)
@@ -152,20 +103,6 @@ n = len(original_data[:, 0])
 obj = Knockoffs()
 knockoffs = obj.GenKnockoffs(n, dim, data_actual)
 
-# Causal skeletion based on prior assumptions/ expert knowledge
-# prior_graph = np.array([[1, 1, 0], [0, 1, 0], [0, 0, 1]])
-prior_graph = np.array([[1, 1, 1, 1, 1], [0, 1, 0, 0, 1], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]])
-
-# Parameters dict
-params = {
-    'num_samples': num_samples,
-    'col': columns,
-    'pred_len': prediction_length,
-    'train_len': training_length,
-    'prior_graph': prior_graph,
-    'dim': dim,
-    'freq': freq
-    }
-
+params = {"dim": dim, "col": columns}
 # Function for estimating causal impact among variables
 deepCause(original_data, knockoffs, model_path, params)
