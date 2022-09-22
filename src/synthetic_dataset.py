1e13bb53ad25bf052cd226fe4425c4103cf6233c
Wasim <wasimahmad.qc@gmail.com>
2022-09-22 20:11:05 +0200

    Synthetic data generation

--- src/synthetic_dataset.py
+++ src/synthetic_dataset.py
@@ -33,10 +33,10 @@ class SyntheticDataset:
         for t in range(10, self.time_steps):
             self.X1.append(self.root[t])
             self.X2.append(C.get('c2') * self.X1[t - Tao.get('t2')] + ey[t])
-            self.X3.append(C.get('c1') ** ((self.X1[t - Tao.get('t1')]) / 2 + ez[t]))
-            self.X4.append(C.get('c3') * self.X1[t - Tao.get('t2')] + er[t])
-            self.X5.append(C.get('c5') * self.X2[t - Tao.get('t1')] + ey[t])
-        return self.X1, self.X2, self.X3, self.X4, self.X5
+            self.X3.append(C.get('c3') * self.X1[t - Tao.get('t3')] + ez[t])                              # C.get('c1') ** ((self.X1[t - Tao.get('t3')]) / 2 + ez[t])
+            # self.X4.append(C.get('c3') * self.X1[t - Tao.get('t3')] + er[t])
+            # self.X5.append(C.get('c5') * self.X2[t - Tao.get('t1')] + ey[t])
+        return self.X1, self.X2, self.X3
 
 
 if __name__ == '__main__':
@@ -58,25 +58,25 @@ if __name__ == '__main__':
 
     _, nice_wave = generate_sine_wave(400, SAMPLE_RATE, DURATION)
     _, noise_wave = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
-    noise_wave = noise_wave * 0.30
+    noise_wave = noise_wave * 0.25
     noise = np.random.normal(0, 1.0, len(nice_wave))
-    root = nice_wave + noise_wave + noise
+    root = nice_wave + noise
 
-    # root = np.random.normal(0, 1.0, 2000)
+    root = np.random.normal(0, 1.0, 2000)
     time_steps, Tref = round(len(root)), 15
-    ey = np.random.normal(0, 0.20, time_steps)
-    ez = np.random.normal(0, 0.25, time_steps)
-    er = np.random.normal(0, 0.15, time_steps)
+    ey = np.random.normal(0, 0.0, time_steps)
+    ez = np.random.normal(0, 0.05, time_steps)
+    er = np.random.normal(0, 0.0, time_steps)
 
-    C = {'c1': 0.95, 'c2': 1.5, 'c3': 2.50, 'c4': 0.75, 'c5': 0.99}           # c2:1.75, c5:1.85
-    Tao = {'t1': 2, 't2': 1, 't3': 4, 't4': 3, 't5': 5, 't6': 6}
+    C = {'c1': 0.90, 'c2': 3.10, 'c3': 1.200, 'c4': 1.00, 'c5': 0.99}           # c2:1.75, c5:1.85
+    Tao = {'t1': 0, 't2': 0, 't3': 0, 't4': 0, 't5': 5, 't6': 6}
     data_obj = SyntheticDataset(root, time_steps, Tref, C, Tao, ey, ez, er)
-    X1, X2, X3, X4, X5 = data_obj.generate_data()
+    X1, X2, X3 = data_obj.generate_data()
 
     # print("SNR (Temperature)", data_obj.SNR(Yts, ez))
 
-    data = {'Z1': X1[150:], 'Z2': X2[150:], 'Z3': X3[150:], 'Z4': X4[150:], 'Z5': X5[150:]}
-    df = pd.DataFrame(data, columns=['Z1', 'Z2', 'Z3', 'Z4', 'Z5'])
+    data = {'Z1': X1[150:], 'Z2': X2[150:], 'Z3': X3[150:]}
+    df = pd.DataFrame(data, columns=['Z1', 'Z2', 'Z3'])
     df.to_csv(r'/home/ahmad/PycharmProjects/deepCausality/datasets/synthetic_datasets/synthetic_data1.csv', index_label=False, header=True)
     print(df.head(100))
     print("Correlation Matrix")
@@ -95,12 +95,12 @@ if __name__ == '__main__':
     ax3.plot(X3[150:1500])
     ax3.set_ylabel("X3")
 
-    ax4 = fig.add_subplot(514)
-    ax4.plot(X4[150:1500])
-    ax4.set_ylabel("X4")
-
-    ax5 = fig.add_subplot(515)
-    ax5.plot(X5[150:1500])
-    ax5.set_ylabel("X5")
+    # ax4 = fig.add_subplot(514)
+    # ax4.plot(X4[150:1500])
+    # ax4.set_ylabel("X4")
+    #
+    # ax5 = fig.add_subplot(515)
+    # ax5.plot(X5[150:1500])
+    # ax5.set_ylabel("X5")
 
     plt.show()
