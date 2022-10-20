333e6b63b1fa6dca379084ed559ec60c16ae750b
Wasim <wasimahmad.qc@gmail.com>
2022-10-20 18:47:13 +0200

    Updated syn data generation

--- src/synthetic_dataset.py
+++ src/synthetic_dataset.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from riverdata import RiverData
 
 
-np.random.seed(3)
+# np.random.seed(1)
 
 
 class SyntheticDataset:
@@ -25,21 +25,23 @@ class SyntheticDataset:
         self.ey = ey
         self.ez = ez
         self.er = er
-        self.X1 = list(np.zeros(15))
-        self.X2 = list(np.zeros(15))
-        self.X3 = list(np.zeros(15))
-        self.X4 = list(np.zeros(15))
-        self.X5 = list(np.zeros(15))
+        self.X1 = list(np.zeros(10))
+        self.X2 = list(np.zeros(10))
+        self.X3 = list(np.zeros(10))
+        self.X4 = list(np.zeros(10))
+        self.X5 = list(np.zeros(10))
 
     def generate_data(self):
 
-        for t in range(15, self.time_steps):
+        for t in range(10, self.time_steps):
             self.X1.append(self.root[t])
             self.X2.append(C.get('c1') * self.X1[t - Tao.get('t1')] + ey[t])
-            self.X3.append(C.get('c2') * self.X1[t - Tao.get('t2')] + ez[t])                                                                  # self.X3.append(C.get('c2') ** ((self.X1[t - Tao.get('t2')]) / 2) + ez[t])
-            self.X4.append(C.get('c3') * self.X2[t - Tao.get('t3')] + er[t])
-            self.X5.append(C.get('c5') * self.X3[t - Tao.get('t2')] + ey[t])
-        return self.X1, self.X2, self.X3, self.X4, self.X5
+            self.X3.append(C.get('c2') * self.X1[t - Tao.get('t2')] + ez[t])    
+            # np.random.seed(4)                                                              # self.X3.append(C.get('c2') ** ((self.X1[t - Tao.get('t2')]) / 2) + ez[t])
+            # self.X4.append(C.get('c3') * self.X2[t - Tao.get('t3')] + er[t])
+            # np.random.seed(5)
+            # self.X5.append(C.get('c5') * self.X3[t - Tao.get('t4')] + ey[t])
+        return self.X1, self.X2, self.X3
 
 
 if __name__ == '__main__':
@@ -61,26 +63,29 @@ if __name__ == '__main__':
 
     _, nice_wave = generate_sine_wave(400, SAMPLE_RATE, DURATION)
     _, noise_wave = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
-    noise_wave = noise_wave * 0.25
+    noise_wave = noise_wave * 0.30
     noise = np.random.normal(0, 1.0, len(nice_wave))
-    root = nice_wave + noise
+    root = nice_wave + noise_wave + noise
 
-    root = np.random.normal(0, 1.0, 2000)
-    time_steps, Tref = round(len(root)), 15
-    ey = np.random.normal(0, 0.01, time_steps)
-    ez = np.random.normal(0, 0.01, time_steps)
-    er = np.random.normal(0, 0.01, time_steps)
+    # root = np.random.normal(0, 1.0, 2000)
+    time_steps, Tref = 1000, 15
+    # np.random.seed(1)
+    ey = np.random.normal(0, 0.35, time_steps)
+    # np.random.seed(2)
+    ez = np.random.normal(0, 0.75, time_steps)
+    # np.random.seed(3)
+    er = np.random.normal(0, 0.30, time_steps)
 
-    C = {'c1': 0.90, 'c2': 1.10, 'c3': 1.85, 'c4': 1.00, 'c5': 0.99}           # c2:1.75, c5:1.85
-    Tao = {'t1': 1, 't2': 1, 't3': 2, 't4': 2, 't5': 5, 't6': 6}
+    C = {'c1': 0.95, 'c2': 1.50, 'c3': 0.85, 'c4': 1.00, 'c5': 0.99}           # c2:1.75, c5:1.85
+    Tao = {'t1': 2 , 't2': 2, 't3': 2, 't4': 3, 't5': 5, 't6': 6}
     data_obj = SyntheticDataset(root, time_steps, Tref, C, Tao, ey, ez, er)
-    X1, X2, X3, X4, X5 = data_obj.generate_data()
+    X1, X2, X3 = data_obj.generate_data()
 
     # print("SNR (Temperature)", data_obj.SNR(Yts, ez))
 
-    data = {'Z1': X1[150:], 'Z2': X2[150:], 'Z3': X3[150:], 'Z4': X4[150:], 'Z5': X5[150:]}
-    df = pd.DataFrame(data, columns=['Z1', 'Z2', 'Z3', 'Z4', 'Z5'])
-    df.to_csv(r'/home/ahmad/PycharmProjects/deepCausality/datasets/synthetic_datasets/synthetic_data2.csv', index_label=False, header=True)
+    data = {'Z1': X1[150:], 'Z2': X2[150:], 'Z3': X3[150:]}
+    df = pd.DataFrame(data, columns=['Z1', 'Z2', 'Z3'])
+    df.to_csv(r'/home/ahmad/PycharmProjects/deepCausality/datasets/synthetic_datasets/synthetic_data10.csv', index_label=False, header=True)
     print(df.head(100))
     print("Correlation Matrix")
     print(df.corr(method='pearson'))
@@ -98,11 +103,11 @@ if __name__ == '__main__':
     ax3.plot(X3[150:1500])
     ax3.set_ylabel("X3")
 
-    ax4 = fig.add_subplot(514)
-    ax4.plot(X4[150:1500])
-    ax4.set_ylabel("X4")
-    #
-    ax5 = fig.add_subplot(515)
-    ax5.plot(X5[150:1500])
-    ax5.set_ylabel("X5")
+    # ax4 = fig.add_subplot(514)
+    # ax4.plot(X4[150:1500])
+    # ax4.set_ylabel("X4")
+    # #
+    # ax5 = fig.add_subplot(515)
+    # ax5.plot(X5[150:1500])
+    # ax5.set_ylabel("X5")
     plt.show()
