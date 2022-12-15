2f225a90dca1c22d018c93f9fc4b7e1f50f966b9
Wasim <wasimahmad.qc@gmail.com>
2022-12-15 16:17:17 +0100

    PARAMS updated for geoclimate data

--- src/parameters.py
+++ src/parameters.py
@@ -166,6 +166,7 @@ def get_geo_params():
         'num_cells': 40,
         'dropout_rate': 0.1,
         'win_size': 1,
+        'slidingwin_size': 100,
         'dim': 5,
         'batch_size': 32,
         'prior_graph': np.array([[1, 1, 0, 1, 0, 1],
