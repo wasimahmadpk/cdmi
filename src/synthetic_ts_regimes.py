464f55d5d867a30ecf0f181dde462f7d4c1c1049
Wasim <wasimahmad.qc@gmail.com>
2023-01-03 17:14:11 +0100

    MUlti-regime time series generation

--- src/synthetic_ts_regimes.py
+++ src/synthetic_ts_regimes.py
@@ -35,21 +35,23 @@ class SyntheticDataset:
             if t < self.time_steps/3:
                 self.X1.append(self.roots[0][t])
                 self.X2.append(C.get('c1') * self.X1[t - Tao.get('t1')] + ey[t])
-                self.X3.append(C.get('c2') ** ((self.X1[t - Tao.get('t2')]) / 2) + ez[t])
-                self.X4.append(C.get('c3') * self.X1[t - Tao.get('t3')] + ex[t])
-                self.X5.append(C.get('c5') * self.X3[t - Tao.get('t4')] + ey[t])
+                self.X3.append(C.get('c2') ** ((self.X1[t - Tao.get('t3')]) / 2) + ez[t])
+                self.X4.append(C.get('c3') * self.X1[t - Tao.get('t1')] + ex[t])
+                self.X5.append(C.get('c5') * self.X4[t - Tao.get('t4')] + C.get('c3') * self.X1[t - Tao.get('t3')] + ey[t])
+
             elif t >= self.time_steps/3 and t < 2*self.time_steps/3:
                 self.X1.append(self.roots[1][t])
                 self.X2.append(C.get('c1') * self.X1[t - Tao.get('t1')] + ex[t])
                 self.X3.append(C.get('c2') ** ((self.X1[t - Tao.get('t2')]) / 2) + ez[t])
                 self.X4.append(C.get('c3') * self.X1[t - Tao.get('t3')] + ey[t])
-                self.X5.append(C.get('c5') * self.X2[t - Tao.get('t4')] + C.get('c5') * self.X3[t - Tao.get('t1')] + ey[t])
+                self.X5.append(C.get('c4') * self.X4[t - Tao.get('t1')] + C.get('c3') * self.X1[t - Tao.get('t1')] + ey[t])
+
             elif t >= 2*self.time_steps/3:
                 self.X1.append(self.roots[2][t])
                 self.X2.append(C.get('c1') * self.X1[t - Tao.get('t1')] + ez[t])
                 self.X3.append(C.get('c2') ** ((self.X1[t - Tao.get('t2')]) / 2) + ey[t])
-                self.X4.append(C.get('c3') * self.X1[t - Tao.get('t3')] + ex[t])
-                self.X5.append(C.get('c5') * self.X2[t - Tao.get('t4')] + ey[t])
+                self.X4.append(C.get('c3') * self.X1[t - Tao.get('t2')] + ex[t])
+                self.X5.append(C.get('c2') * self.X4[t - Tao.get('t1')] + C.get('c3') * self.X1[t - Tao.get('t3')] + ey[t])
 
         return self.X1, self.X2, self.X3, self.X4, self.X5
 
@@ -83,43 +85,44 @@ if __name__ == '__main__':
         roots.append(root)
 
     # root = np.random.normal(0, 1.0, 2000)
-    time_steps, Tref = 3333, 15
+    time_steps, Tref = 1111, 15
 
-    ex = np.random.normal(0, 0.10, time_steps)
-    ey = np.random.normal(0, 0.15, time_steps)
-    ez = np.random.normal(0, 0.20, time_steps)
+    ex = np.random.normal(0, 0.15, time_steps)
+    ey = np.random.normal(0, 0.05, time_steps)
+    ez = np.random.normal(0, 0.10, time_steps)
 
-    C = {'c1': 0.95, 'c2': 1.50, 'c3': 0.85, 'c4': 1.00, 'c5': 0.99}           # c2:1.75, c5:1.85
-    Tao = {'t1': 2, 't2': 1, 't3': 4, 't4': 3, 't5': 5, 't6': 6}
+    C = {'c1': 0.95, 'c2': 1.25, 'c3': 0.85, 'c4': 1.00, 'c5': 1.11}           # c2:1.75, c5:1.85
+    Tao = {'t1': 1, 't2': 2, 't3': 4, 't4': 3, 't5': 5, 't6': 6}
     data_obj = SyntheticDataset(roots, time_steps, Tref, C, Tao, ex, ey, ez)
     X1, X2, X3, X4, X5 = data_obj.generate_data()
 
     data = {'Z1': X1[10:], 'Z2': X2[10:], 'Z3': X3[10:], 'Z4': X4[10:], 'Z5': X5[10:]}
     df = pd.DataFrame(data, columns=['Z1', 'Z2', 'Z3', 'Z4', 'Z5'])
+    df = df.apply(prep.normalize)
     df.to_csv(r'/home/ahmad/PycharmProjects/deepCausality/datasets/synthetic_datasets/synthetic_data_regimes.csv', index_label=False, header=True)
     print(df.shape)
-    print(df.head(100))
+    print(df.head(10))
     print("Correlation Matrix")
     print(df.corr(method='pearson'))
 
     fig = plt.figure()
     ax1 = fig.add_subplot(511)
-    ax1.plot(X1[10:])
+    ax1.plot(X1[10:1000])
     ax1.set_ylabel('X1')
 
     ax2 = fig.add_subplot(512)
-    ax2.plot(X2[10:])
+    ax2.plot(X2[10:1000])
     ax2.set_ylabel("X2")
 
     ax3 = fig.add_subplot(513)
-    ax3.plot(X3[10:])
+    ax3.plot(X3[10:1000])
     ax3.set_ylabel("X3")
 
     ax4 = fig.add_subplot(514)
-    ax4.plot(X4[10:])
+    ax4.plot(X4[10:1000])
     ax4.set_ylabel("X4")
     
     ax5 = fig.add_subplot(515)
-    ax5.plot(X5[10:])
+    ax5.plot(X5[10:1000])
     ax5.set_ylabel("X5")
     plt.show()
