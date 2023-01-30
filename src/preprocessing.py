47176abafc5a146a14040353462f23614692075d
Wasim <wasimahmad.qc@gmail.com>
2023-01-30 18:39:43 +0100

    Geodata analysis updated

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -155,16 +155,19 @@ def load_geo_data():
     #    'snow_load', 'humidity', 'glob_radiaton', 'strain_ew_uncorrected',
     #    'strain_ns_uncorrected', 'strain_ew_corrected', 'strain_ns_corrected',
     #    'tides_ew', 'tides_ns']
-    vars = ['DateTime', 'gw_mb', 'gw_sg', 'gw_west', 'gw_south', 'strain_ns_corrected']
-    # vars = ['DateTime', 'temperature_outside', 'pressure_outside', 'wind_x', 'snow_load', 'strain_ns_corrected']
+    vars = ['DateTime', 'gw_mb', 'gw_sg', 'gw_west', 'gw_south', 'strain_ew_corrected', 'strain_ns_corrected']
+    # vars = ['DateTime', 'temperature_outside', 'pressure_outside', 'wind_x', 'wind_y', 'strain_ew_corrected', 'strain_ns_corrected']
     data = pd.read_csv(path, usecols=vars)
     
     # Read spring and summer season geo-climatic data
-    # mask = (data['DateTime'] > '2014-11-01') & (data['DateTime'] <= '2015-06-30')
-    mask = (data['DateTime'] > '2015-07-01') & (data['DateTime'] <= '2015-12-30')
-    data = data.loc[mask]
+    start_date = '2014-11-01'
+    end_date = '2015-06-30'
+    # mask = (data['DateTime'] > '2014-11-01') & (data['DateTime'] <= '2015-05-28')  # '2015-06-30') Regime 1
+    # mask = (data['DateTime'] > '2015-05-01') & (data['DateTime'] <= '2015-10-30')  # Regime 2
+    # data = data.loc[mask]
     data = data.fillna(method='pad')
     data = data.set_index('DateTime')
+    data = data[start_date: end_date]
     data = data.apply(normalize)
 
     return data
@@ -206,7 +209,7 @@ def load_flux_data():
     # nee = fluxnet['NEE_VUT_50']
     ogpp = fluxnet['GPP_NT_VUT_50']
     oreco = fluxnet['RECO_NT_VUT_50']
-    # ************* LOad FLUXNET2015 data ************************
+    # ************* LOad FLUXNET2015 data ***************
 
     rg = normalize(down_sample(org, win_size))
     temp = normalize(down_sample(otemp, win_size))
@@ -226,14 +229,14 @@ def load_flux_data():
 
 
 def load_syn_data():
-    # Load synthetic data *************************
+    #****************** Load synthetic data *************************
     data = pd.read_csv("../datasets/synthetic_datasets/synthetic_data.csv")
     df = data.apply(normalize)
     return df
 
 
 def load_multiregime_data():
-    # Load synthetic data *************************
+    # *******************Load synthetic data *************************
     df = pd.read_csv("../datasets/synthetic_datasets/synthetic_data_regimes.csv")
     # df = df.apply(normalize)
     return df
