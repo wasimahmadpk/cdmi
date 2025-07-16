d56421585277e97a12e8d414b1a4a38ea1420234
Wasim <wasimahmad.qc@gmail.com>
2025-07-16 16:01:29 +0200

    ablation CDMI

--- src/parameters.py
+++ src/parameters.py
@@ -89,7 +89,7 @@ def get_syn_params():
         'num_cells': 20,          # 50
         'dropout_rate': 0.1,
         'step_size': 3,
-        'num_sliding_win': 50,
+        'num_sliding_win': 30,
         'dim': 5,
         'alpha': 0.10,
         'batch_size': 32,
